8d560f06a8c788372c207f540fa3c259772bb9ea
heat_nan <964465194@qq.com>
2016-06-29 00:48:34 +0800

    modify start shell

--- WebServer.sh
+++ WebServer.sh
@@ -5,14 +5,34 @@
 
 appname=WebServer
 
-start() {
+ start() {
 
 	echo "Now will start the $appname" 
+	pid=$(pidof $appname)
+	if [ -n "$pid" ]; then
+		echo "the $appname is running,start failed,pid is $pid"
+		exit 0
+	fi
+	echo "Now run the $appname"
+	nohup ./$appname
+	apppid=$(pidof $appname)
+	echo "the $appname is start successful! the pid is $apppid"
 }
 
 stop() {
 	
 	echo "Now will stop the $appname"
+	pid=$(pidof $appname)
+	if [ -z "$pid" ]; then
+		echo "the app is not running"
+		exit 
+	fi
+	kill -9 $pid
+	
+	pid=$(pidof $appname)
+	if [ -z "$pid" ];then
+		echo " kill successful "
+	fi
 }
 
 case "$1" in
