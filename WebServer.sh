490276351196a5792a650d4319ab7934bb5290b1
heatnan <964465194@qq.com>
2018-08-17 05:14:12 +0000

    delete nohup.out && fix WebServer.sh

--- WebServer.sh
+++ WebServer.sh
@@ -5,31 +5,40 @@
 
 appname=WebServer
 
- start() {
+start() 
+{
 
 	echo "Now will start the $appname" 
+
 	pid=$(pidof $appname)
+
 	if [ -n "$pid" ]; then
 		echo "the $appname is running,start failed,pid is $pid"
 		exit 0
 	fi
+
 	echo "Now run the $appname"
-	nohup ./$appname
+
+	nohup ./$appname  > /dev/null  2>&1 &
 	apppid=$(pidof $appname)
 	echo "the $appname is start successful! the pid is $apppid"
 }
 
-stop() {
+stop() 
+{
 	
 	echo "Now will stop the $appname"
+
 	pid=$(pidof $appname)
 	if [ -z "$pid" ]; then
 		echo "the app is not running"
 		exit 
 	fi
+
 	kill -9 $pid
 	
 	pid=$(pidof $appname)
+
 	if [ -z "$pid" ];then
 		echo " kill successful "
 	fi
